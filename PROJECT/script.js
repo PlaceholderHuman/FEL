4e782af0976fd39646c593326e67c052652011f7
PlaceholderHuman <masonmcm@hawaii.edu>
2024-10-09 13:07:54 -1000

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,18 @@
+function clicked(){
+    document.getElementById("test").innerText = "Clicked"
+}
+function future(){
+    location.href = "future.html"
+}
+function about(){
+    location.href = "index.html"
+}
+function concept(){
+    location.href = "concept.html"
+}
+function hist(){
+    location.href = "history.html"
+}
+function present(){
+    location.href = "present.html"
+}
