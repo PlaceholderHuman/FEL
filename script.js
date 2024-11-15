03f2e8729a83bf38768169cc09bee9f86bb0b55d
PlaceholderHuman <masonmcm@hawaii.edu>
2024-11-15 10:57:53 -1000

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function people(){
 function hist(){
     location.href = "history.html"
 }
+
+function remap(){
+    var setter = document.getElementsByClassName("threefirst")[0];
+    var follower = document.getElementsByClassName("three")[0];
+    var setterpos = setter.getBoundingClientRect();
+    follower.style.right =((setter.offsetWidth - follower.offsetWidth)/2 + "px");
+    console.log(setter.offsetWidth, follower.offsetWidth, (setter.offsetWidth - follower.offsetWidth)/2 + "px")
+    console.log(setterpos.left, follower.style.left,  follower.getBoundingClientRect().left)
+}
